3f9e1a8d7a46bf56e227895e9eeef3df09f2a92e
SanderJ <sander.jonkers+github@gmail.com>
2015-05-24 21:35:49 +0200

    een try except erin

--- nogeentje.py
+++ nogeentje.py
@@ -3,6 +3,20 @@ print "hiermee moet het lukken"
 for x in range(1,11):
     print x
 
+if True:
+    print "het is waar"
+    a=1
+    b=a
+    c=2
+
+
 print "Klaar"
 
 print "dacht je dat?"
+try:
+    print 1/0
+except:
+    print "Dat ging niet"
+    pass
+
+print "einde"
